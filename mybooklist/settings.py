8700eab00a645348d527f17267e02b3937906a07
Shota Amashukeli <gfyusobt@gmail.com>
2016-12-02 23:15:33 +0400

    Bad request

--- mybooklist/settings.py
+++ mybooklist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://fathomless-cliffs-13908.herokuapp.com','0.0.0.0']
 
 # Application definition
 
@@ -120,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
