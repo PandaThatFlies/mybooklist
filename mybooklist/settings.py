de656a96d034632ae0db4bd73b42426a0bce1341
Shota Amashukeli <gfyusobt@gmail.com>
2016-12-02 11:58:49 +0400

    Configuring Database

--- mybooklist/settings.py
+++ mybooklist/settings.py
@@ -74,19 +74,19 @@ WSGI_APPLICATION = 'mybooklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
-#heroku dayabase
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
+
+#heroku dayabase
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
