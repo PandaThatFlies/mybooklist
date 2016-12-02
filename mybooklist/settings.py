a1972a267daa2971e9e738aac484cdcd7c75b90e
Shota Amashukeli <gfyusobt@gmail.com>
2016-12-02 17:03:57 +0400

    Whitenoise

--- mybooklist/settings.py
+++ mybooklist/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
